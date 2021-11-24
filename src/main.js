d39000f1713453165a51d16181359e003822b99f
Leey <leeynykeramr@gmail.com>
2021-11-23 19:20:54 -0500

    pruebas de inicio

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
+import VueAxios from 'vue-axios';
+import axios from "axios";
+import '@babel/polyfill'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
+import "@/assets/css/general.css"
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
+
